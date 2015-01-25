22c72ee325b0d873ffa9373ead5a98b228cf5ee4
Rahul <rahul.1186@gmail.com>
2015-01-24 21:30:15 -0600

    Crime incident analysis using hadoop
    Analysing crime incident data using different configurations of hadoop.

---
+++ Region-definition-2/CrimeIncidents.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class CrimeIncidents{
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable>{
+		private Text region = new Text();
+		private final static IntWritable one = new IntWritable(1);
+
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+			String currentLine = value.toString();
+			if(!(currentLine.startsWith("Crime ID"))){
+				String[] data = currentLine.split(",");
+				if(!(data[4].equals("") && data[5].equals("")) && data.length>=8){
+					String eastCord = data[4];
+					String northCord = data[5]; 
+					String crimeType = data[7];
+					region.set(eastCord.substring(0,3) + ", " + northCord.substring(0,3) + ", " + crimeType);
+					context.write(region, one);
+				}
+			}
+		}
+	}
+
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable>
+	{
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+			int crimes = 0;
+
+			for (IntWritable i : values){
+				crimes += i.get();
+			}
+			context.write(key, new IntWritable(crimes));
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Exception{
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "CrimeIncidents");
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setJarByClass(CrimeIncidents.class);
+		job.setMapperClass(Map.class);
+		job.setCombinerClass(Reduce.class);
+		job.setReducerClass(Reduce.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.waitForCompletion(true);
+	}
+}
